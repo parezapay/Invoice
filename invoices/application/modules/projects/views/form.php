beadbcb8cacaaae01c89b78e8e78b22dfcb53ead
motionarchive <motionarchive@motionarchive-02>
2019-06-28 16:02:55 +0530

    add class and styles

--- invoices/application/modules/projects/views/form.php
+++ invoices/application/modules/projects/views/form.php
@@ -4,6 +4,7 @@
     });
 </script>
 
+<div class="table-shadow" style="margin-top: 30px;">
 <form method="post">
 
     <input type="hidden" name="<?php echo $this->config->item('csrf_token_name'); ?>"
@@ -31,3 +32,4 @@
     </div>
 
 </form>
+</div>
